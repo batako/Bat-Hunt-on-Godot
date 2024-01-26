923a20688d599c356125efd8e3fcbcfa2311c929
batako <guri725ren213@gmail.com>
2024-01-26 19:07:13 +0900

    ノードを画面中央に移動させる処理を共通処理化

--- src/Common.gd
+++ src/Common.gd
@@ -1,1 +1,12 @@
+extends Node2D
+
 const ENEMY_INSTANCE_BASE_NAME = "Enemy"
+
+# 画面中央にノードを移動
+func move_to_screen_center():
+	# ビューポートのサイズを取得
+	var viewport_size = get_viewport().size
+	# ビューポートの中心点を計算
+	var center = viewport_size / 2
+	# ノードの位置をビューポートの中心に設定
+	position = center
